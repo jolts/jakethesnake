acfe28ed30faa2b79cc6066243220d249e9f45e4
jolts <johan@sharpcode.se>
2009-01-31 17:47:03 +0100

    added first skeleton-port of Snake_Engine

---
+++ snake_engine.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby -w
+
+require 'sdl'
+
+class Snake_Engine
+  def initalize(width, height, fullscreen)
+    $image_buffer = Hash.new
+    state = Abstract_State.new
+    if(SDL::Init(SDL::INIT_VIDEO) == -1)
+      print ("Can't init SDL: #{SDL::SDL_GetError}")
+      exit 1
+    end
+
+    if fullscreen
+      flags = SDL::SDL_SWSURFACE or SDL::SDL_FULLSCREEN
+    else
+      flags = SDL::SDL_SWSURFANCE
+    end
+
+    if screen == nil
+      print ("Can't set video mode: #{SDL::SDL_GetError}")
+      SDL::SDL_Quit
+      exit 1
+      state = nil
+      tick_interval = 50
+    end
+    
+    def set_state(state)
+      
+  end
+end
+
+
