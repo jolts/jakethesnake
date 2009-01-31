af74a20922b05b932d4cef4351c70ee564e6df42
jolts <johan@sharpcode.se>
2009-01-31 19:31:42 +0100

    added gameoverstate

--- lib/gameover_state.rb
+++ lib/gameover_state.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
+require 'abstract_state'
+require 'snake_engine'
+require 'menu_state'
 
 module JakeTheSnake
-  class GameoverState
+  class GameoverState < Abstract_State
+    def key_pressed(key)
+      if (key == SDL::SDKL_RETURN) 
+        # When user presses return we go back to menu
+        get_engine.set_state(Menu_State.new)
+      end
+    end
+  end
+end
