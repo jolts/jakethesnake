1a02b8a64b0a046241226ead4abe1708ae5e245f
jolts <johan@sharpcode.se>
2009-01-31 19:28:21 +0100

    abstract_state added

--- lib/abstract_state.rb
+++ lib/abstract_state.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
+require 'snake_engine'
+require 'sdl'
 
 module JakeTheSnake
   class AbstractState
+    attr_accessor :engine
+    def initalize
+      engine = Snake_Engine.new
+      self.engine = 0
+    end
+
+    def draw(surface)
+    end
+
+    def clock_tick
+    end
+
+    def key_pressed(key)
+    end
+  end
+end
+
+
+
+
+
