5e80d6265bf15e4e9cf72c074f77b4b74beae899
jolts <johan@sharpcode.se>
2009-01-31 21:08:45 +0100

    Finished collision handler class

--- lib/collision_handler.rb
+++ lib/collision_handler.rb
@@ -25,7 +25,38 @@ module JakeTheSnake
       end
     end
 
-    def is_self_snake_collision()
+    def is_self_snake_collision(snake)
+      # Checks whether one part of the snake
+      # collides with another part.
+      snake.each do |first_part|
+        snake.each do |second_part+1|
+          if first_part.x == second_part.x or 
+              first_part.y == second_part.y
+            true
+          else
+            false
+          end
+        end
+      end
+    end
+
+    def is_player1_player2_collision(player1, player2)
+      if player1[0].x == player2[0].x and player1[0].y == player2[0].y
+        3 # Head to head collision
+      end
+
+      player1.each do |player1|
+        if player1.x == player2[0].x and player1.y == player2[0].y
+          2 # Player 2 looose
+        end
+
+        player2.each do |player2|
+          if player2.x == player1[0].x and player2.y == player1[0].y
+            1 # Player 1 loose
+          end
+        end
+        0 # No collision
+      end
     end
   end
 end
