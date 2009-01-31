ddffbea50e45bc26a78716c167f58957943a9c18
jolts <johan@sharpcode.se>
2009-01-31 22:28:35 +0100

    finished obstacles class, inhereting from sprite

--- lib/obstacles.rb
+++ lib/obstacles.rb
@@ -1,4 +1,17 @@
 #!/usr/bin/env ruby
+require 'sprite'
 
 module JakeTheSnake
-  class Obstacles
+  class Obstacles < Sprite
+    def initalize(image)
+      self.image = image
+    end
+
+    def draw(screen)
+      apply_surface(x, y, self.image, screen)
+    end
+
+    private
+    @@image = SDL::Surface
+  end
+end
