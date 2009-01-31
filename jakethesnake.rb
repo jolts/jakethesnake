805e8754f43007b1338eb8616196d41b89d7864b
jolts <johan@sharpcode.se>
2009-01-31 18:02:20 +0100

    update

--- jakethesnake.rb
+++ jakethesnake.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby -w
 require 'lib/abstract_state'
 require 'lib/collision_handler'
 require 'lib/gameover_state'
@@ -10,3 +11,17 @@ require 'lib/single_state'
 require 'lib/snake_engine'
 require 'lib/snake'
 require 'lib/sprite'
+require 'sdl'
+
+module JakeTheSnake
+  class Game
+    def initalize
+      end
+    
+    def run
+      engine = Snake_Engine.new(640, 480, false)
+      state = Menu_State.new
+      engine.set_state(state)
+      engine.set_tick_interval(100)
+      engine.loop
+end
