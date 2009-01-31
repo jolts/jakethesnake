c1d43e986ee7bd55f7cde2aa85c1ebcfd530e03a
jolts <johan@sharpcode.se>
2009-02-01 00:25:22 +0100

    fixed gigs mess in main.rb

--- main.rb
+++ main.rb
@@ -12,7 +12,6 @@ require 'lib/single_state'
 require 'lib/snake_engine'
 require 'lib/snake'
 require 'lib/sprite'
-
 require 'sdl'
 
 module JakeTheSnake
@@ -32,9 +31,9 @@ module JakeTheSnake
     begin
       run
     rescue StandardError => err
-      $stderr.puts "\033[0;31mStandardError! \n\n\033[0m"+err.message.each_line do { |lines| puts "> "+lines } end
+      $stderr.puts "\033[0;31mStandardError! \n\n\033[0m"+err.message.each_line do |lines| puts "> "+lines  end
     rescue Exception => exp
-      $stderr.puts "\033[0;31mException! \n\n\033[0m"+exp.message.each_line do { |lines| puts "> "+lines } end
+      $stderr.puts "\033[0;31mException! \n\n\033[0m"+exp.message.each_line do  |lines| puts "> "+lines  end
     end
   end
 end
